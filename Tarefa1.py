504c9e7267de78c1e5934f8ccfb855a0248adcd2
Victor Milaré <victormilare@outlook.com>
2023-01-18 19:03:15 -0300

    Update Tarefa1.py
    Refactor.

--- Tarefa1.py
+++ Tarefa1.py
@@ -4,50 +4,24 @@
 # Caso queira ver o código no repositório, acesse https://github.com/CostaMateus7/tarefaAlgoritmosPhyton
 # Eu evito comentar muito o código para manter uma boa prática de programação. Caso tenha dificuldade na leitura das variáveis e funções, favor passar um feedback para que eu possa melhorar nesse sentido. 
 
+import math
 
 nome = input('Escreva seu nome: ')
 print('Bem vindo a loja do Mateus, {}!'.format(nome))
 
+productPrice = input('Digite o valor do produto: ')
 
-
-while True:
-
-  productPrice = float(input('Digite o valor do produto: '))
-  if(productPrice<0):
-    continue
-
-  productQuant = float(input('Escreva a quantidade de produtos: '))
-
-  if((productQuant % 2) == 0 or (productQuant + 1) % 2 == 0):
-    
-    if(0 <= productQuant < 11):
-      subTotal = productPrice * productQuant
-      total = subTotal + 30
-      print('O valor sem o frete foi: R${}'.format(subTotal))
-      print('O valor com o frete foi: R${}'.format(total))
-      break
-    elif(11 <= productQuant < 101):
-      subTotal = productPrice * productQuant
-      total = subTotal + 60
-      print('O valor sem o frete foi: R${}'.format(subTotal))
-      print('O valor com o frete foi: R${}'.format(total))
-      break
-    elif(101 <= productQuant < 1001):
-      subTotal = productPrice * productQuant
-      total = subTotal + 120
-      print('O valor sem o frete foi: R${}'.format(subTotal))
-      print('O valor com o frete foi: R${}'.format(total))
-      break
-    elif(productQuant >= 1001):
-      subTotal = productPrice * productQuant
-      total = subTotal + 240
-      print('O valor sem o frete foi: R${}'.format(subTotal))
-      print('O valor com o frete foi: R${}'.format(total))
-      break
+if not productPrice.isnumeric() or float(productPrice) < 0:
+    print('Valor de produto inválido!')
+else:
+  productQuant = input('Escreva a quantidade de produtos: ')
+  if productQuant.isdigit() and int(productQuant) > 0 :
+    productQuant = int(productQuant)
+    productPrice = float(productPrice)
+    freteFactor = math.floor(math.log10(max(1, abs(productQuant-1))))
+    subTotal = productPrice * productQuant
+    total = subTotal + 30 * 2 ** (freteFactor)
+    print('O valor sem o frete foi: R${}'.format(subTotal))
+    print('O valor com o frete foi: R${}'.format(total))
   else:
     print('Quantidade de produto inválido!')
-    continue
-
-
-
-
